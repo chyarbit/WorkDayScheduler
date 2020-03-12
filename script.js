57c88c996cf00079bba0c5874f048efaef0ec320
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-03-12 18:29:19 -0500

    finished notes on js file including added feature

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
+// command to ensure that the Javascript does not run until all of the HTML has rendered 
 $(document).ready(function() {
-  // listen for save button clicks
+  // when the save button is clicked
   $(".saveBtn").on("click", function() {
-    // get nearby values
+    // get the value that was entered into the description
     var value = $(this).siblings(".description").val();
+    // get the value of the time that is the parent value of the description
     var time = $(this).parent().attr("id");
 
-    // save in localStorage
+    // save the values in localStorage
     localStorage.setItem(time, value);
   });
 
+  // Create a function hourUpdater that if empty will perform the following
   function hourUpdater() {
     // get current number of hours
     var currentHour = moment().hours();
 
-    // loop over time blocks
+    // loop over time blocks of the day and will perform the following
     $(".time-block").each(function() {
+      // define the variable blockHour as getting the integer value of the hour ID
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
       // check if we've moved past this time
       if (blockHour < currentHour) {
+        // if the conditional is true, add the integer value of the hour ID to the past class
         $(this).addClass("past");
       } 
+      // if the block hour and current hour are equal to each other both in type and value
       else if (blockHour === currentHour) {
+        // remove the integer from the past class
         $(this).removeClass("past");
+        // add the integer to the present class
         $(this).addClass("present");
       } 
+      // if neither of the two conditions listed above are true,
       else {
+        // remove the integer from the past class
         $(this).removeClass("past");
+        // remove the integer from the present class
         $(this).removeClass("present");
+        // add the integer to the future class
         $(this).addClass("future");
       }
     });
   }
-
+  // call the function hourUpdater to run
   hourUpdater();
 
   // set up interval to check if current time needs to be updated
@@ -52,3 +64,6 @@ $(document).ready(function() {
   // display current day on page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
+
+// Added feature will be the current time right below the date
+  $("#currentTime").text(moment().format("hh:mm"));
